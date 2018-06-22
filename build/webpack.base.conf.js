2f306c27593cccdc7800dada8c1d10593f905ae7
yushare <582718643@qq.com>
2018-06-22 15:59:39 +0800

    install babel-polyfill

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -13,7 +13,7 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/index.js'
+    app: ['babel-polyfill', './src/index.js']
   },
   externals:{
     'vue': 'Vue',
--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,24 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted () {
+    let p = new Promise((resolve, reject) => {
+      if (1>0) {
+        resolve('ysy')
+      }
+    })
+    p.then(res => {
+      this.warn(res);
+    })
+  },
+  methods: {
+    warn (res) {
+      setTimeout(() => {
+        let i = 1;
+        !!i && alert(res)
+      }, 1000);
+    }
   }
 }
 </script>
